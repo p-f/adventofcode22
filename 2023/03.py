c8ee1748136192db9aba69969d5026baed0d8773
Philip Fritzsche <p-f@users.noreply.github.com>
2023-12-03 13:41:56 +0100

    2023 Day 3 Part 2

--- 2023/03.py
+++ 2023/03.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+from functools import reduce
 
 Part = namedtuple('Part', ['nr', 'line', 'col'])
 
@@ -19,12 +20,18 @@ def find_parts(l: str, line: int, outlist: list) -> None:
 	if partnr is not None:
 		outlist.append(Part(partnr, line, len(l) - len(partnr)))
 
-def is_part_adj(field: list, part: Part) -> bool:
+def is_part_adj(field: list, part: Part, parts_for_gear: dict) -> bool:
 	lines, cols = len(field), len(field[0])
 	def is_symbol(linenr, colnr):
 		s = field[linenr][colnr]
 		#print(linenr, colnr, s)
-		return (not s.isdigit()) and s != '.'
+		is_symb = (not s.isdigit()) and s != '.'
+		if s == '*':
+			gearpos = (linenr, colnr,)
+			if gearpos not in parts_per_gear:
+				parts_per_gear[gearpos] = set()
+			parts_per_gear[gearpos].add(part)
+		return is_symb
 	#print('Checking ', part)
 	for l in range(max(0, part.line - 1), min(lines, part.line + 2)):
 		for c in range(max(0, part.col - 1), min(cols, part.col + len(part.nr) + 1)):
@@ -42,8 +49,15 @@ with open('03.input', 'r') as f:
 		find_parts(l, line, parts)
 		line += 1
 	part_sum = 0
+	parts_per_gear = dict()
 	for p in parts:
-		print(p, 'adjacent?', adj:=is_part_adj(field, p))
+		print(p, 'adjacent?', adj:=is_part_adj(field, p, parts_per_gear))
 		if adj:
 			part_sum += int(p.nr)
 	print('Part Sum:', part_sum)
+	sum_gear_ratio = 0
+	for gear, parts in parts_per_gear.items():
+		if len(parts) == 2:
+			ratio = reduce(int.__mul__, map(lambda p:int(p.nr), parts))
+			sum_gear_ratio += ratio
+	print('Ratio Sum', sum_gear_ratio)
