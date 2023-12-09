f3721ad150186a99a59a7a39412361e19268b783
Philip Fritzsche <p-f@users.noreply.github.com>
2023-12-09 12:18:49 +0100

    2023 Day 8 Part 2

--- 2023/08.py
+++ 2023/08.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import re
+from functools import reduce
+from math import lcm
 
 instr = None
 network = dict()
 
-net_pattern = re.compile(r'([A-Z]+) = \(([A-Z]+)\, ([A-Z]+)\)')
+net_pattern = re.compile(r'([A-Z12]+) = \(([A-Z12]+)\, ([A-Z12]+)\)')
 
 with open('08.input', 'r') as f:
     instr = f.readline().strip()
@@ -15,11 +17,28 @@ with open('08.input', 'r') as f:
         st, l, r = m.group(1,2,3)
         network[st] = (l, r,)
 
-steps = 0
-pos = 'AAA'
-while pos != 'ZZZ':
-    for i in instr:
-        pos = network[pos][0 if i == 'L' else 1]
-        steps += 1
-        if pos == 'ZZZ': break
-    print(f'After {steps} steps at {pos}')
+def count_steps(startpos, endpos):
+    steps = 0
+    pos = startpos
+    while not pos.endswith(endpos):
+        for i in instr:
+            pos = network[pos][0 if i == 'L' else 1]
+            steps += 1
+            if pos.endswith(endpos): break
+        print(f'After {steps} steps at {pos} - {startpos} -> {endpos}')
+    return steps
+
+print('Part 1', count_steps('AAA', 'ZZZ'))
+
+ghosts = [k for k in network if k[-1] == 'A']
+
+print(ghosts)
+
+ghost_steps = []
+
+for g in ghosts:
+    s = count_steps(g, 'Z')
+    print(f'Steps for {g}: {s}') 
+    ghost_steps.append(s)
+
+print('Part 2:', reduce(lcm, ghost_steps))
