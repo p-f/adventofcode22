95b308d3de8804eccc80945c0f88b5272cefef8c
Philip Fritzsche <p-f@users.noreply.github.com>
2024-12-03 15:19:53 +0100

    2024 Day 3 Part 2

--- 2024/03.py
+++ 2024/03.py
@@ -16,3 +16,20 @@ for line in code:
        result += int(l) * int(r)
 
 print(result)
+
+# Part 2
+instr = re.compile("(mul|do|don't)\((([0-9]+),([0-9]+))?\)")
+
+result = 0
+do = True
+
+for line in code:
+    for i, arg, l, r in instr.findall(line):
+        if i == "mul" and arg and do:
+            result += int(l) * int(r)
+        elif i == "do" and not arg:
+            do = True
+        elif i == "don't" and not arg:
+            do = False
+
+print(result)
