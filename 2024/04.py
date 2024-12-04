42b95626f411acd186f694ec1cfc8e72eba219ff
Philip Fritzsche <p-f@users.noreply.github.com>
2024-12-04 17:00:07 +0100

    Day 4 Part 2

--- 2024/04.py
+++ 2024/04.py
@@ -47,3 +47,32 @@ for l in range(lines):
                 xmas_count += 1
 
 print(xmas_count)
+
+# Part 2
+
+def gridget(l, c):
+    if in_grid(l, c):
+        return grid[l][c]
+    else:
+        return None
+
+def is_as(corner1, corner2):
+    return (corner1 == 'M' and corner2 == 'S') or (corner1 == 'S' and corner2 == 'M')
+
+def search_xmas_2d(l, c):
+    if grid[l][c] != 'A':
+        return False
+    top_left  = gridget(l - 1, c - 1)
+    top_right = gridget(l - 1, c + 1)
+    bot_left  = gridget(l + 1, c - 1)
+    bot_right = gridget(l + 1, c + 1)
+    return is_as(top_left, bot_right) and is_as(top_right, bot_left)
+
+xmas_count = 0
+
+for l in range(lines):
+    for c in range(columns):
+        if search_xmas_2d(l, c):
+            xmas_count += 1
+
+print(xmas_count)
