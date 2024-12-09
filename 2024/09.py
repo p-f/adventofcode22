4bb8264d01c8c949efb5f9f7db7994cc14b5d8a2
Philip Fritzsche <p-f@users.noreply.github.com>
2024-12-10 00:44:10 +0100

    2024 Day 9 Part 2 Overly complicated

--- 2024/09.py
+++ 2024/09.py
@@ -52,9 +52,93 @@ def get_blocks():
     if rem_tail:
         yield tail // 2, rem_tail
 
-hash_val, hash_pos = 0, 0
-for block_id, block_count in get_blocks():
-    hash_val += block_id * sum(range(hash_pos, hash_pos + block_count))
-    hash_pos += block_count
+def hash_blocks(blocks):
+    hash_val, hash_pos = 0, 0
+    for block_id, block_count in blocks:
+        hash_val += block_id * sum(range(hash_pos, hash_pos + block_count))
+        hash_pos += block_count
+    return hash_val
 
-print(hash_val)
+print(hash_blocks(get_blocks()))
+
+# Part 2
+
+def chunk_fs():
+    for i, c in enumerate(fs_data):
+        if i % 2 == 0:
+            yield (i // 2, int(c))
+        else:
+            yield [None, int(c)]
+
+fs = list(chunk_fs())
+max_id = fs[-1][0] or fs[-2][0]
+
+def compress_fs(fs_queue): # Unused
+    i = 0
+    while i < len(fs_queue):
+        chunk = fs_queue[i]
+        if chunk[0] is not None: # File
+            yield chunk
+            i += 1
+            continue
+        new_chunksize = chunk[1]
+        merged_chunks = 1
+        for j in range(i + 1, len(fs_queue)):
+            chunk2 = fs_queue[j]
+            if chunk2[0] is not None:
+                break
+            merged_chunks += 1
+            new_chunksize += chunk2[1]
+        yield [chunk[0], new_chunksize]
+        i += merged_chunks
+
+def defrag(fs_queue_in):
+    head = 1
+    moved = [False for _ in range(max_id + 1)]
+    fs_queue = fs_queue_in
+    def is_free(idx):
+        return fs_queue[idx][0] is None and fs_queue[idx][1] > 0
+    def move(fs_idx, new_idx):
+        #print("Move", fs_idx, "to", new_idx)
+        chunk_to_move = fs_queue[fs_idx]
+        chunk_size = chunk_to_move[1]
+        # Replace chunk with empty space
+        fs_queue[fs_idx] = [None, chunk_size]
+        # Shrink free space
+        fs_queue[new_idx][1] -= chunk_size
+        # Re-insert chunk and mark as moved
+        fs_queue.insert(new_idx, chunk_to_move)
+        moved[int(chunk_to_move[0])] = True
+    while True:
+        while not is_free(head):
+            head += 1
+            if head >= len(fs_queue):
+                return
+        # Find block to move
+        any_moved = False
+        for idx in range(len(fs_queue) - 1, head, -1):
+            chunk_id, chunk_size = fs_queue[idx]
+            if chunk_id is None or chunk_size == 0: continue
+            if moved[chunk_id]: continue
+            chunk_moved = False
+            # Find fitting space
+            for new_idx in range(head, idx):
+                if not is_free(new_idx): continue
+                chunk_candidate = fs_queue[new_idx]
+                if chunk_candidate[1] >= chunk_size:
+                    chunk_moved = True
+                    move(idx, new_idx)
+                    break
+            moved[chunk_id] = True
+            if chunk_moved:
+                any_moved = True
+                break
+        if not any_moved: return
+        #print(len(fs_queue), head)
+
+defrag(fs)
+def iter_fs(f):
+    for chunk, chunk_size in f:
+        yield chunk or 0, chunk_size
+print(hash_blocks(iter_fs(fs)))
+
