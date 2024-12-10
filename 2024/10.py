9eac2890f32d05f3de62661b7b8ddd6fdbce732a
Philip Fritzsche <p-f@users.noreply.github.com>
2024-12-10 13:56:57 +0100

    2024 Day 10 Part 2

--- 2024/10.py
+++ 2024/10.py
@@ -26,19 +26,21 @@ def find_peaks(current_coord):
     current_height = height(current_coord)
     if current_height == 9:
         return [current_coord]
-    peaks_reachable = set()
+    peaks_reachable = list()
     for d in Direction:
         next_pos = current_coord + d.value
         if in_field(next_pos) and height(next_pos) == current_height + 1:
             for p in find_peaks(next_pos):
-                peaks_reachable.add(p)
+                peaks_reachable.append(p)
     return peaks_reachable
 
 all_peaks = 0
+all_paths = 0
 for r in range(n_rows):
     for c in range(n_cols):
         if height(coord := coordinates(r, c)) == 0:
-            hr = len(find_peaks(coord))
-            #print("Head", r, c, hr)
-            all_peaks += hr
+            peaks = find_peaks(coord)
+            all_peaks += len(set(peaks))
+            all_paths += len(peaks)
 print(all_peaks)
+print(all_paths)
