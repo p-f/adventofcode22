15ee83cbebb2dbed974458c30765ceea120d86f1
Philip Fritzsche <p-f@users.noreply.github.com>
2024-12-10 13:52:25 +0100

    2024 Day 10 Part 1

---
+++ 2024/10.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from santashelpers import coordinates, Direction, to_row_col
+
+field = []
+with open('10.input', 'r') as f:
+    while l := f.readline():
+        row = []
+        for c in l.strip():
+            row.append(int(c) if c != '.' else -1)
+        field.append(row)
+
+n_rows = len(field)
+n_cols = len(field[0])
+
+def height(coord):
+    r, c = to_row_col(coord)
+    return field[r][c]
+
+def in_field(coord):
+    r, c = to_row_col(coord)
+    return r >= 0 and c >= 0 and r < n_rows and c < n_cols
+
+def find_peaks(current_coord):
+    current_height = height(current_coord)
+    if current_height == 9:
+        return [current_coord]
+    peaks_reachable = set()
+    for d in Direction:
+        next_pos = current_coord + d.value
+        if in_field(next_pos) and height(next_pos) == current_height + 1:
+            for p in find_peaks(next_pos):
+                peaks_reachable.add(p)
+    return peaks_reachable
+
+all_peaks = 0
+for r in range(n_rows):
+    for c in range(n_cols):
+        if height(coord := coordinates(r, c)) == 0:
+            hr = len(find_peaks(coord))
+            #print("Head", r, c, hr)
+            all_peaks += hr
+print(all_peaks)
