87bf10176ef08f76d16227a22f57716a858536bb
Philip Fritzsche <p-f@users.noreply.github.com>
2025-01-01 13:44:28 +0100

    2024 Day 19 Part 2

--- 2024/19.py
+++ 2024/19.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from santashelpers import chunks
+from functools import lru_cache
 
 all_towels = []
 designs = None
@@ -13,16 +14,22 @@ with open("19.input", "r") as f:
 shortest_towel_design = min(map(len, all_towels))
 longest_towel_design = max(map(len, all_towels))
 
+@lru_cache
 def try_make_design(target_design :str):
-    if target_design in all_towels or len(target_design) == 0: return True
-    if len(target_design) < shortest_towel_design: return False
+    if len(target_design) == 0: return 1
+    if len(target_design) < shortest_towel_design: return 0
+    possible_design_count = 0
     for t in all_towels:
         if target_design.startswith(t):
-            if try_make_design(target_design[len(t):]):
-                return True
-    return False
+            possible_design_count += try_make_design(target_design[len(t):])
+    return possible_design_count
 
 possible_designs = 0
+possible_design_combinations = 0
 for t_d in designs:
-    if try_make_design(t_d): possible_designs += 1
+    #print("Design", t_d)
+    if m_d := try_make_design(t_d): possible_designs += 1
+    possible_design_combinations += m_d
+    #print("Combibations", possible_design_combinations)
 print(possible_designs)
+print(possible_design_combinations)
