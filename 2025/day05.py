64a9f201502e4f7d08ec508dcaf24bbbf5d637e4
Philip Fritzsche <p-f@users.noreply.github.com>
2025-12-05 10:27:31 +0100

    2025 Day 5 Part 2

--- 2025/day05.py
+++ 2025/day05.py
@@ -20,7 +20,24 @@ def main():
     for inc in incredients:
         if any(rf <= inc and inc <= rt for rf, rt in ranges):
             fresh_count += 1
-    print(fresh_count)
+    print("Part 1", fresh_count, sep="\t")
+    # Part 2
+    ranges.sort()
+    total_count: int = 0
+    last_start: int
+    last_end: int
+    last_start, last_end = ranges[0]
+    for r_s, r_e in ranges:
+        if last_start <= r_s <= last_end:
+            # Start overlaps previous
+            if r_e > last_end:
+                last_end = r_e
+        else:
+            total_count += 1 + (last_end - last_start)
+            last_start = r_s
+            last_end = r_e
+    total_count += 1 + (last_end - last_start)
+    print("Part 2", total_count, sep="\t")
 
 if __name__ == "__main__":
     main()
