2bf6f461a0317b0097941bed83a496d2b7bfe071
Philip Fritzsche <p-f@users.noreply.github.com>
2025-12-10 14:17:21 +0100

    2025 Day 10 Part 2

--- 2025/day10.py
+++ 2025/day10.py
@@ -1,8 +1,12 @@
 #!/bin/env python3
 
 from dataclasses import dataclass
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
+import math
 from helpers.io import read_lines
+
+import numpy as np
+from scipy.optimize import linprog
 
 type bitset = int
 
@@ -13,6 +17,8 @@ def _set_bit(index: int) -> bitset:
 class Maschine:
     lights: bitset
     buttons: list[bitset]
+    buttons_list: list[tuple[int, ...]]
+    joltage_req: list[int]
 
     @classmethod
     def read(cls, line: str) -> Maschine:
@@ -26,12 +32,17 @@ class Maschine:
                 assert l == ".", (lights, i, l)
         # Read buttons
         buttons: list[bitset] = []
+        buttons_lists: list[tuple[int, ...]] = []
         for btn_str in components[1:-1]:
             button: bitset = 0
-            for btn_id in map(int, btn_str.strip("()").split(",")):
+            btn_list: tuple[int, ...] = tuple(map(int, btn_str.strip("()").split(",")))
+            for btn_id in btn_list:
                 button |= _set_bit(btn_id)
             buttons.append(button)
-        return Maschine(lights, buttons)
+            buttons_lists.append(btn_list)
+        # Read joltage requirements
+        joltage_req: list[int] = list(map(int, components[-1].strip("{}").split(",")))
+        return Maschine(lights, buttons, buttons_lists, joltage_req)
 
 def _part1_naive(machine: Maschine) -> int:
     # Each button can at most be pressed once, pressing twice will lead back to original state
@@ -50,10 +61,43 @@ def read_machines() -> list[Maschine]:
         machines.append(Maschine.read(l))
     return machines
 
+def _part2_naive(machine: Maschine) -> int:
+    print("Finding for ", machine)
+    gcd: int = math.gcd(*machine.joltage_req)
+    max_presses: int = sum(machine.joltage_req) // gcd
+    num_counters: int = len(machine.joltage_req)
+    for num_presses in range(1, max_presses + 1):
+        for buttons in combinations_with_replacement(machine.buttons_list, num_presses):
+            counters: list[int] = [0 for _ in range(num_counters)]
+            for button in buttons:
+                for index in button:
+                    counters[index] += gcd
+            if all(counters[i] == machine.joltage_req[i] for i in range(num_counters)):
+                return num_presses
+    raise RuntimeError("Not found for", machine)
+
+def _part2(machine: Maschine) -> int:
+    fact_list: list[list[int]] = []
+    for eq_nr in range(len(machine.joltage_req)):
+        coeff: list[int] = []
+        for btn in machine.buttons_list:
+            if eq_nr in btn:
+                coeff.append(1)
+            else:
+                coeff.append(0)
+        fact_list.append(coeff)
+    # Right side: final joltages
+    a = np.array(fact_list)           # Variables: number of button presses
+    b = np.array(machine.joltage_req) # Expected joltages
+    c = np.array([1 for _ in range(len(machine.buttons_list))]) # Optimize for sum of parameters
+    integrality = [1 for _ in range(len(machine.buttons_list))] # Parameters have to be all ints
+    res = linprog(c, A_eq=a, b_eq=b, integrality=integrality)
+    return int(sum(res.x))
+
 def main():
     machines: list[Maschine] = read_machines()
-    # Test
     print("Part 1", sum(_part1_naive(m) for m in machines))
+    print("Part 2", sum(_part2(m) for m in machines))
 
 if __name__ == "__main__":
     main()
