7068adb544cab9ed8f9b997bb39d4f93b981af53
Philip Fritzsche <p-f@users.noreply.github.com>
2025-12-11 11:52:48 +0100

    2025 Day 11 Part 2

--- 2025/day11.py
+++ 2025/day11.py
@@ -12,14 +12,18 @@ def read_rack() -> dict[str, list[str]]:
         rack[left] = right.split()
     return rack
 
-@timethis
-def part1(rack: dict[str, list[str]]) -> int:
+def _find_sources(rack: dict[str, list[str]]) -> dict[str, list[str]]:
     sources: dict[str, list[str]] = dict()
     for device, outs in rack.items():
         for out in outs:
             if out not in sources:
                 sources[out] = []
             sources[out].append(device)
+    return sources
+
+@timethis
+def part1(rack: dict[str, list[str]]) -> int:
+    sources: dict[str, list[str]] = _find_sources(rack)
     starts_reached: int = 0
     frontier: Counter[str] = Counter(["out"])
     next_frontier: Counter[str] = Counter()
@@ -34,9 +38,30 @@ def part1(rack: dict[str, list[str]]) -> int:
         next_frontier.clear()
     return starts_reached
 
+@timethis
+def part2(rack: dict[str, list[str]]) -> int:
+    sources: dict[str, list[str]] = _find_sources(rack)
+    starts_reached: int = 0
+    frontier: Counter[tuple[str, bool, bool]] = Counter([("out", False, False,)])
+    next_frontier: Counter[tuple[str, bool, bool]] = Counter()
+    while frontier:
+        for front in frontier:
+            front_machine, visited_dac, visited_fft = front
+            for source in sources.get(front_machine, ()):
+                if source == "svr" and visited_dac and visited_fft:
+                    starts_reached += frontier[front]
+                else:
+                    visited_fft_new: bool = visited_fft or (source == "fft")
+                    visited_dac_new: bool = visited_dac or (source == "dac")
+                    next_frontier[(source, visited_dac_new, visited_fft_new)] += frontier[front]
+        frontier, next_frontier = next_frontier, frontier
+        next_frontier.clear()
+    return starts_reached
+
 def main():
     rack: dict[str, list[str]] = read_rack()
     print(part1(rack))
+    print(part2(rack))
 
 if __name__ == "__main__":
     main()
